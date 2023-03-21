d60c25ed438a4f25027fbaf4321f8fa827ed2837
Christian <christiantm93@gmail.com>
2023-03-21 17:10:39 +0100

    Refactor metodos por ChristianTM

--- src/Car.java
+++ src/Car.java
@@ -2,11 +2,15 @@ public class Car {
     public int velocity;
 
 
-    public void acelerate(int aceleration) {
+    public void acelerate_ChristianTM(int aceleration) {
         velocity += aceleration;
     }
 
-    public void desacelerate(int desaceleration) {
+    public void desacelerate_ChristianTM(int desaceleration) {
+
         velocity -= desaceleration;
+        if(velocity < 0) {
+            velocity =0;
+        }
     }
 }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,30 +3,30 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_to_create_car_velocity_is_0() {
+    public void test_to_create_car_velocity_is_0_ChristianTM() {
         Car newCar = new Car();
         Assertions.assertEquals(0, newCar.velocity);
     }
 
     @Test
-    public void test_acelerate_car_velocity_increase() {
+    public void test_acelerate_car_velocity_increase_ChristianTM() {
         Car newCar = new Car();
-        newCar.acelerate(30);
+        newCar.acelerate_ChristianTM(30);
         Assertions.assertEquals(30, newCar.velocity);
     }
 
     @Test
-    public void test_desacelerate_car_velocity_decrease() {
+    public void test_desacelerate_car_velocity_decrease_ChristianTM() {
         Car newCar = new Car();
         newCar.velocity = 50;
-        newCar.desacelerate(20);
+        newCar.desacelerate_ChristianTM(20);
         Assertions.assertEquals(30, newCar.velocity);
     }
     @Test
-    public void test_desacelerate_car_velocity_decrease_cant_under_0() {
+    public void test_desacelerate_car_velocity_decrease_cant_under_0_ChristianTM() {
         Car newCar = new Car();
         newCar.velocity = 50;
-        newCar.desacelerate(80);
+        newCar.desacelerate_ChristianTM(80);
         Assertions.assertEquals(0, newCar.velocity);
     }
 }
