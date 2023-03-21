28c1a9dcd3a38b0bfa608a18e08a196758dad9b6
Christian <christiantm93@gmail.com>
2023-03-21 16:27:14 +0100

    Test 2 car with velocity, new field velocity in Car

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,3 @@
 public class Car {
+    public int velocity;
 }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
     public void test_to_create_car_velocity_is_0() {
         Car newCar = new Car();
+        Assertions.assertEquals(0, newCar.velocity);
     }
 }
